ca896e88a5839e9097c291cca10f56503bae6007
Kirill <100080763+Snakeinweb@users.noreply.github.com>
2023-08-05 14:56:22 +0700

    Обновили код с заданием

--- T3_1_home_work.py
+++ T3_1_home_work.py
@@ -1,4 +1,25 @@
 
 
-a = input("go: ")
-print ("hello world", a)
+print('Çäğàâñòâóéòå')
+
+prif = 'ëåò'
+
+who_animal = input('Êòî âàø ïèòîìåö? ')
+
+name = input('Èìÿ âàøåãî ïèòîìöà? ')
+
+age = int(input('Âîçğàñò âàøåãî ïèòîìöà? '))
+
+# â äàëüíåéøåì ìîæíî îáğàáîòàòü äî äğóãèõ "ëåò"
+
+if age == 1:
+
+    prif = 'ãîä'
+
+else:
+
+    if age >= 2 and age <= 4:
+
+        prif = 'ãîäà'
+
+print('İòî',who_animal,'ïî êëè÷êå "'+name+'". Âîçğàñò:',age,prif)
